684061512fc9bb782f95bcb9f1c1a1951f4d285e
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-17 18:45:12 +0100

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from './Home.jsx'
+import ItemDetailPage from "./ItemDetailPage.jsx";
 const App = () => {
+    const [data, setData] = useState(null) 
+    const urlAPI = 'http://localhost:3000'
+    const fetchData = async()=> {
+      try{
+        const response = await fetch(urlAPI)
+        const resData = await response.json()
+        setData(resData)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    useEffect(()=>{
+      fetchData()
+    }, [])
 
   return (
-  <></>      
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Inicio</Link>
+          </nav>
+          {data === null ? (
+            <div>cargando...</div>
+          ) :
+          <Routes>
+            <Route path="/" element={<Home data={data} />} />
+            {data.map(item=> (
+              <Route key={item._id} path={`/${item._id}`} element={<ItemDetailPage item={item}/>} />
+            )
+
+            )}
+          </Routes>
+          }
+          
+        </div>
+      </Router>
   )
 };
 
